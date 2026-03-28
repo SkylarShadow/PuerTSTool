affc53ba7a9269c26edd54cfb256bf06c15e8b06
SKYlarS <851428179@qq.com>
2026-03-28 12:50:49 +0800

    恢复修改让ts持有stub

--- TypeScript/Framework/Utils/mixin.ts
+++ TypeScript/Framework/Utils/mixin.ts
@@ -1,6 +1,6 @@
 import * as UE from "ue";
 import { blueprint } from "puerts";
-export default function mixin(blueprintPath: string, objectTakeByNative = true) {
+export default function mixin(blueprintPath: string, objectTakeByNative = false) {
     return function <T extends UE.Object>(target: new () => T) {
         const ucls = UE.Class.Load(blueprintPath);
         const BlueprintClass = blueprint.tojs(ucls);
